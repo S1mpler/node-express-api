c531afddbc0666a6f4c706942e432a50c7bdc449
DreiDogs <stas.dior@gmail.com>
2019-01-03 15:27:33 +0100

    Added basic structure
    route, middleware, libs

---
+++ routes/my-route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+let router = express.Router();
+
+router.get("/", (req, res) => {
+  // do stuff here
+});
+
+/// EXPORT //////////////////////////////////////////////
+module.exports = router;
+/////////////////////////////////////////////////////////
